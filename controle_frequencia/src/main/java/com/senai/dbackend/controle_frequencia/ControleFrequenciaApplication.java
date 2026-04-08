fb57301bdd4c260d1f57cb57c2ff2b21e9b5c8b9
caio-barbosa0 <pet0163157@aluno.firjansenaisesi.com.br>
2026-04-08 16:28:38 -0300

    enviando api

---
+++ controle_frequencia/src/main/java/com/senai/dbackend/controle_frequencia/ControleFrequenciaApplication.java
@@ -0,0 +1,14 @@
+package com.senai.dbackend.controle_frequencia;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ControleFrequenciaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ControleFrequenciaApplication.class, args);
+		System.out.println("saracuruna é bom");
+	}
+
+}
